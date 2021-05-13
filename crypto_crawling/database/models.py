cc1320c6add4cbae44135a678131b63f860f7757
AdamIanRoberts <adamianroberts@gmail.com>
2021-05-13 16:44:16 +0100

    Add id and amend datetime columns types

--- crypto_crawling/database/models.py
+++ crypto_crawling/database/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Float, JSON, String
+from sqlalchemy import Column, DateTime, Float, JSON, String, Integer
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.ext.declarative.api import DeclarativeMeta
 
@@ -9,15 +9,15 @@ Base: DeclarativeMeta = declarative_base()
 class Trades(Base):
     __tablename__ = "trades"
 
-    format = Column(String, nullable=True)
-    timestamp = Column(String, nullable=False, index=True, primary_key=True)
-    receipt_timestamp = Column(String, nullable=False, index=True)
+    id = Column(Integer, primary_key=True)
+    timestamp = Column(DateTime, nullable=False, index=True)
+    receipt_timestamp = Column(DateTime, nullable=False, index=True)
     feed = Column(String, nullable=False, index=True)
     symbol = Column(String, nullable=False, index=True)
     side = Column(String, nullable=False)
     amount = Column(Float, nullable=False)
     price = Column(Float, nullable=False)
-    id = Column(String, nullable=True)
+    trade_id = Column(String, nullable=True)
     order_type = Column(String, nullable=True)
 
     def __repr__(self):
@@ -28,9 +28,9 @@ class Trades(Base):
 class Book(Base):
     __tablename__ = "book"
 
-    format = Column(String, nullable=True)
-    timestamp = Column(String, nullable=False, index=True, primary_key=True)
-    receipt_timestamp = Column(String, nullable=False, index=True)
+    id = Column(Integer, primary_key=True)
+    timestamp = Column(DateTime, nullable=False, index=True)
+    receipt_timestamp = Column(DateTime, nullable=False, index=True)
     feed = Column(String, nullable=False, index=True)
     symbol = Column(String, nullable=False, index=True)
     data = Column(JSON, nullable=False)
